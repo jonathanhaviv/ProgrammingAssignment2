fd84aec049be9dc8a380474c22fae27953499eb7
Jonathan Haviv <53003885+jonathanhaviv@users.noreply.github.com>
2020-05-06 22:03:34 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Takes a square matrix that can be inverted and returns
-## a list of functions to set the matrix, 
-## invert the matrix, 
-## set the inverse,
-## get the inverse
-
+## Creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+        
+        ## Method to set the matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        
+        ## Method to get the matrix
         get <- function() x
-        setmatrix <- function(solve) m <<- solve
-        getmatrix <- function() m
+        
+        ## Method to set the inverse matrix
+        setinverse <- function(inverse) m <<- inverse
+        
+        ## Method to get the inverse matrix
+        getinverse <- function() m
+        
+        ## Populate list of functions neede
         list(set = set, get = get, 
-             setmatrix = setmatrix,
-             getmatrix = getmatrix)
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
 
-## Write a short comment describing this function
-## Takes the output of makeCacheMatrix() and returns the inverted matrix
+## Finds the inverted matrix either through solve or retrieves it from the cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getmatrix()
+        m <- x$getinverse()
+        
+        ## If the inverse is found in the cache returns it from cache
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        
+        ## Gets the matrix
         data <- x$get()
+        
+        ## Get the inverse of the matrix using solve()
         m <- solve(data, ...)
-        x$setmatrix(m)
+        
+        ## Sets the inverse of the matrix to m
+        x$setinverse(m)
+        
+        # Returns inverted matrix
         m
 }
